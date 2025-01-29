caafef09521d4bf09b8280725466eb866462a48c
Ilana Molcanova <ilana.roscina@gmail.com>
2025-01-29 16:45:48 +0000

    Added delivery_team seed and added it to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -19,8 +19,10 @@ select
     , p.PRODUCTNAME
     , p.SUBCATEGORY
     ,{{ markup('ORDERSELLINGPRICE', 'ORDERCOSTPRICE') }} as markup
+    , d.delivery_team
 FROM {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c on o.CUSTOMERID = c.CUSTOMERID
 left join {{ ref('raw_product') }} as p on o.PRODUCTID = p.PRODUCTID
+left join {{ ref('delivery_team') }} as d on o.SHIPMODE = d.shipmode
 
 -- {{limit_data_in_dev('o.ORDERDATE')}}
