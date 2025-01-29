bea47762bb6d3d6c3fa404a812c4d12c2958be05
Ilana Molcanova <ilana.roscina@gmail.com>
2025-01-29 14:06:20 +0000

    Developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,20 @@
+select
+-- from raw orders
+	  o.ORDERID
+	, o.ORDERDATE
+	, o.SHIPDATE
+	, o.SHIPMODE
+    , o.ORDERSELLINGPRICE - o.ORDERCOSTPRICE as ORDER_PROFIT
+    , o.ORDERCOSTPRICE
+    , o.ORDERSELLINGPRICE
+-- from raw customers
+    , c.CUSTOMERNAME
+    , c.SEGMENT
+    , c.COUNTRY
+-- from raw product
+    , p.CATEGORY
+    , p.PRODUCTNAME
+    , p.SUBCATEGORY
+FROM {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c on o.CUSTOMERID = c.CUSTOMERID
+left join {{ ref('raw_product') }} as p on o.PRODUCTID = p.PRODUCTID
