fc64e42df388bb7f0d14bdc865c65ba166905ccb
Ilana Molcanova <ilana.roscina@gmail.com>
2025-01-29 16:38:11 +0000

    Added dbt_utils into packages.yml. Used generate_surrogate_key in stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,6 +1,7 @@
 select
 -- from raw orders
-	  o.ORDERID
+    {{ dbt_utils.generate_surrogate_key(['o.ORDERID', 'o.CUSTOMERID', 'o.PRODUCTID']) }} as sk_orders
+	, o.ORDERID
 	, o.ORDERDATE
 	, o.SHIPDATE
 	, o.SHIPMODE
@@ -22,4 +23,4 @@ FROM {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c on o.CUSTOMERID = c.CUSTOMERID
 left join {{ ref('raw_product') }} as p on o.PRODUCTID = p.PRODUCTID
 
-{{limit_data_in_dev('o.ORDERDATE')}}
+-- {{limit_data_in_dev('o.ORDERDATE')}}
